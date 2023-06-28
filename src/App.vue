2552c95a4083a11985031e683a49c701c61332d4
qingxiangNo1 <741140559@qq.com>
2023-06-28 15:59:36 +0800

    提交格式化代码

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-       <h1>我是app根组件</h1>
+  <div class="box">
+    <h1>我爱你祖国</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-
+for(let i=0;i<100;i++){
+  console.log(i);
+}
 </script>
 
 <style scoped>
-
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
 </style>
